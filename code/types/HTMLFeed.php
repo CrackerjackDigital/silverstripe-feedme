8dd2f8f3e9b41ff3c97a05e32fa92951e205ebd2
Steve Wake <stephen.wake@careers.govt.nz>
2015-11-30 14:10:30 +1300

    Added use link as external ID and refactor HTMLFeed to make more re-usable

--- code/types/HTMLFeed.php
+++ code/types/HTMLFeed.php
@@ -5,6 +5,8 @@
  * feed instance as the xpath to items and the map from items to domain model
  * will probably always be instance specific unlike ATOM and RSS where the
  * xpath is a defined standard.
+ *
+ * @property DOMNodeList $items
  */
 abstract class FeedMeHTMLFeed extends FeedMeHTMLFeedIterator {
 
@@ -57,8 +59,10 @@ abstract class FeedMeHTMLFeed extends FeedMeHTMLFeedIterator {
 
 
 	/**
+	 * Map from DOMElement to neutral field map.
+	 *
 	 * @param DOMElement $itemDataFromFeed
-	 * @return array
+	 * @return array map of neutral fields to values.
 	 */
 	public function map($itemDataFromFeed) {
 		$map = array_fill_keys(
@@ -105,17 +109,9 @@ abstract class FeedMeHTMLFeed extends FeedMeHTMLFeedIterator {
 					$tagName = $child->tagName;
 
 					if (isset($nodeToPropMap[$tagName]) && $nodeToPropMap[$tagName]) {
-						switch ($tagName) {
-						case 'a':
-							// links set the link and an md5 hash of it for the ExternalID.
-							$map[$this->fieldMap[$nodeToPropMap[$tagName]]] = $child->getAttribute('href');
-
-							if (empty($map[$this->fieldMap[FeedMeItemModelExtension::ExternalIDFieldName]])) {
-								// tested so if this set 'really' then don't overwrite
-								$map[$this->fieldMap[FeedMeItemModelExtension::ExternalIDFieldName]] = md5($child->getAttribute('href'));
-							}
-							break;
-						default:
+						// see if any special handling for this tag
+						if (!$this->child($child, $nodeToPropMap, $map)) {
+							// no special handling so do use default map (textContent).
 							// concatenate as we could say have multiple 'p' tags.
 							$map[$this->fieldMap[$nodeToPropMap[$child->tagName]]] .= (string) $child->textContent;
 						}
@@ -123,6 +119,35 @@ abstract class FeedMeHTMLFeed extends FeedMeHTMLFeedIterator {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Handle any specific processing for a tag. If config.use_link_as_external_tag then if there is
+	 * no external id set an md5 hash of the link (a tag) will be used as the post ExternalID for
+	 * resolving insert/update action.
+	 *
+	 * @param             $tagName
+	 * @param \DOMElement $child
+	 * @param array       $nodeToPropMap
+	 * @return bool         true if tag was handled, false otherwise
+	 */
+	protected function child(DOMElement $child, array $nodeToPropMap, array &$map) {
+		$tagName = $child->tagName;
+
+		switch ($tagName) {
+		case 'a':
+			// links set the link and an md5 hash of it for the ExternalID.
+			$map[$this->fieldMap[$nodeToPropMap[$tagName]]] = $child->getAttribute('href');
+
+			if (empty($map[$this->fieldMap[FeedMeItemModelExtension::ExternalIDFieldName]])) {
+				if (Config::inst()->get(get_called_class(), 'use_link_hash_as_external_id')) {
+					// tested so if this set 'really' then don't overwrite
+					$map[$this->fieldMap[FeedMeItemModelExtension::ExternalIDFieldName]] = md5($child->getAttribute('href'));
+				}
+			}
+			return true;
+		}
+		return false;
 	}
 
 }
